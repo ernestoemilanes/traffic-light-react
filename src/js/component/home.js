70ca8fd354ef5fa8bf4d6723bd968fbd69338d0d
Ernesto <ernestoemilanes@gmail.com>
2020-10-17 18:23:12 -0400

    first commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [color, setColor] = useState(["red"]);
+	useEffect(
+		() => {
+			const interval = setInterval(() => {
+				color === "red"
+					? setColor("green")
+					: color === "yellow"
+						? setColor("red")
+						: color === "green"
+							? setColor("yellow")
+							: null;
+			}, 5000);
+			return () => clearInterval(interval);
+		},
+		[color]
+	);
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<ul className="trafficLight">
+				<li
+					onClick={() => setColor("red")}
+					className={color === "red" ? "selected" : ""}
+				/>
+				<li
+					onClick={() => setColor("yellow")}
+					className={color === "yellow" ? "selected" : ""}
+				/>
+				<li
+					onClick={() => setColor("green")}
+					className={color === "green" ? "selected" : ""}
+				/>
+			</ul>
 		</div>
 	);
 }
